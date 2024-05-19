eba1f318fbe3feaef6b582a7186a0ea18157cb4f
macbook air <mvoodivood@yandex.ru>
2024-05-19 16:49:43 +0300

    change font

--- components/sections/hero/benefits.tsx
+++ components/sections/hero/benefits.tsx
@@ -5,24 +5,20 @@ const Benefits = () => {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
           <div className="flex justify-center bg-secondary-bg rounded-5xl p-4">
             <div className="flex gap-3 items-center">
-              <h3 className="text-[75px] leading-[100%]">20</h3>
-              <p className="text-secondary">
-                Лет опыт руководителей направлений
-              </p>
+              <div className="text-[75px] font-bold">20</div>
+              <p className="text-secondary">Лет опыт руководителей направлений</p>
             </div>
           </div>
           <div className="flex justify-center bg-secondary-bg rounded-5xl p-4">
             <div className="flex gap-3 items-center">
-              <h3 className="text-[75px] leading-[100%]">с2010</h3>
+              <div className="text-[75px] font-bold">c2010</div>
               <p className="text-secondary">Года мы работаем с клиентами</p>
             </div>
           </div>
           <div className="flex justify-center border-2 border-primary rounded-5xl p-4">
             <div className="flex gap-3 items-center">
-              <h3 className="text-[75px] leading-[100%]">110</h3>
-              <p className="text-secondary">
-                Клиентов разного масштаба работают с нами
-              </p>
+              <div className="text-[75px] font-bold">110</div>
+              <p className="text-secondary">Клиентов разного масштаба работают с нами</p>
             </div>
           </div>
         </div>
