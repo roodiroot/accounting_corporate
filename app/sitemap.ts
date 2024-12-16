ce5325ee8091f71c2afb964d776ea00acbb54c2d
maxim <borisov130490@gmail.com>
2024-12-16 22:45:54 +0300

    fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://partner-biznes.ru/servises",
+      url: "https://partner-biznes.ru/services",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
